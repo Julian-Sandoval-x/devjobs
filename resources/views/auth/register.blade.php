524c8ea337b90c04495dc3d9500a0a64c1084418
Julian Sandoval <juliangodina17@gmail.com>
2026-05-24 16:56:16 -0700

    Envio de email para verificacion de cuenta

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -14,6 +14,15 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('¿Qué tipo de cuenta deseas en DevJobs')" />
+            
+            <select> 
+                
+            </select>
+
         </div>
 
         <!-- Password -->
@@ -30,7 +39,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Repetir Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -39,14 +48,24 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+         <div class="flex justify-between my-5">
+            
+            <x-link
+                :href="route('login')"
+            >
+                ¿Ya tienes cuenta? Inicia Sesión
+            </x-link>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <x-link 
+                :href="route('password.request')"
+            >
+                ¿Olvidaste tu contraseña?
+            </x-link>
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ ('Crear Cuenta') }}
+        </x-primary-button>
+
     </form>
 </x-guest-layout>
